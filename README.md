e8ff7bfeafdc4ba1b895f077c7c2ba18f32d0455
Aldur <adrianodl@hotmail.it>
2015-06-02 11:39:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# United Tweet Analyzer
+Machine Learning - Web & Social classes, joint project, 2015.
+
+## Authors
+Adriano Di Luzio & Danilo Francati
---
+++ UnitedTweetsAnalyzer/src/main/java/com/github/aldurd392/UnitedTweetsAnalyzer/App.java
@@ -0,0 +1,13 @@
+package com.github.aldurd392.UnitedTweetsAnalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
